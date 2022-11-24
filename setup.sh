56c0abc222583601a50e9bff02523ee2f3657fd8
juju <78792729+iTechJustynaKowalczyk@users.noreply.github.com>
2022-11-24 14:19:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,14 +9,6 @@ cd $TMPDIR
 for script in ~/.dotfiles/scripts/*; do
   bash "$script"
 done
-tasks:
-  - name: Setup SSH
-    before: |
-      ssh-keyscan github.com >> "${HOME}/.ssh/known_hosts"
-      if [[ ! -z "${id_rsa}" ]]; then
-        echo "${id_rsa}" | base64 --decode > "${HOME}/.ssh/id_rsa"
-        chmod 0600 "${HOME}/.ssh/id_rsa"
-      fi
 
 cd $CURRENT
 
